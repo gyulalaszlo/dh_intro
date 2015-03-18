4ecf4105dc0aecd9e72cf952c6fc3b5e1e91f9df
Miles Gibson <gyula.laszlo.gm@gmail.com>
2015-03-18 14:54:03 +0100

    ex 3.3

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,10 @@ class User < ActiveRecord::Base
   has_one :role
 
 
+  # only allow users with a valid name
+  validates_presence_of :name
+  validates_length_of :name, minimum: 2
+
   # Query all permissions of a user as in exercise 3.2
   def all_permissions
     permissions + role.permissions
